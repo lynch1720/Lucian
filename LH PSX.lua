eb528c6ec7666e6893c0aaaa862fe5f8e3ff9df7
lynch1720 <120999525+lynch1720@users.noreply.github.com>
2022-12-20 13:30:26 +0800

    Add files via upload

---
+++ LH PSX.lua
@@ -0,0 +1,130 @@
+local DiscordLib = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/discord%20lib.txt")()
+ 
+local win = DiscordLib:Window("Lynch Hub")
+ 
+local serv = win:Server("Scripts", "")
+ 
+local btns = serv:Channel("Blox Fruits")
+
+btns:Button("Quick Exp(Visual)", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptpastebin/raw/main/MaxFast"))()
+DiscordLloadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()ib:Notification("Notification", "Executed", "COOL")
+end)
+
+
+btns:Button("Zamex Hub", function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Sixnumz/ZamexMobile/main/Zamex_Mobile.lua'))()
+DiscordLloadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()ib:Notification("Notification", "RaceClicker", "Okay!")
+end)
+ 
+btns:Seperator()
+ 
+btns:Button("CFrame Hub", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CFrame3310/CFrameHub/main/BloxFruit_Mobile.lua"))()
+DiscordLloadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()ib:Notification("Notification", "RaceClicker", "Okay!")
+end)
+ 
+btns:Seperator()
+ 
+btns:Button("Zen Hub", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Kaizenofficiall/ZenHub/main/bfmobile", true))()
+DiscordLloadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()ib:Notification("Notification", "RaceClicker", "Okay!")
+end)
+ 
+btns:Seperator()
+ 
+btns:Button("Mokuro", function()
+loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()
+DiscordLloadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()ib:Notification("Notification", "RaceClicker", "Okay!")
+end)
+ 
+btns:Seperator()
+ 
+btns:Button("Hoho Hub", function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI'))()
+DiscordLloadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()ib:Notification("Notification", "RaceClicker", "Okay!")
+end)
+
+btns:Seperator()
+
+btns:Button("Zaque Hub", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Mei2232/ZaqueHub/main/Zaque%20Hub"))()
+DiscordLloadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()ib:Notification("Notification", "Executed", "COOL")
+end)
+
+btns:Seperator()
+
+btns:Button("ThunderZ", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ThunderZ-HUB/HUB/main/Mobile-V2"))()
+DiscordLloadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()ib:Notification("Notification", "Executed", "COOL")
+end)
+
+local btns = serv:Channel("Pet Simulator X")
+
+btns:Button("James Hub", function()
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/jmesfo0/RobloxScripts/main/psx-jmes.lua"))()
+DiscordLloadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()ib:Notification("Notification", "Executed", "COOL")
+end)
+
+btns:Seperator()
+
+btns:Button("Project-WD", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/Project-WD/main/Main.lua"))()
+DiscordLloadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()ib:Notification("Notification", "Executed", "COOL")
+end)
+
+local btns = serv:Channel("Lumber Tycoon 2")
+
+btns:Button("Noob Army", function()
+loadstring(game:HttpGet('https://getexploits.com/key-system/',true))('https://da.com/936657404291084298/1006220505583460352/Script.txt')
+DiscordLloadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()ib:Notification("Notification", "Executed", "COOL")
+end)
+
+btns:Seperator()
+
+btns:Button("Butter Hub", function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Butterisgood/butter-hub/main/butters%20recode-obfuscated.lua'))()
+DiscordLloadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()ib:Notification("Notification", "Executed", "COOL")
+end)
+
+local btns = serv:Channel("Shindo Life")
+
+btns:Button("Projec-WD", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/Project-WD/main/Main.lua"))()
+DiscordLloadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()ib:Notification("Notification", "Executed", "COOL")
+end)
+
+btns:Seperator()
+
+btns:Button("Life V2", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SxnwDev/Premier/main/Free-Premier.lua", true))()
+DiscordLloadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()ib:Notification("Notification", "Executed", "COOL")
+end)
+
+btns:Seperator()
+
+btns:Button("Infinite Spin", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Xiovr/Roblox/main/SL.lua"))()
+DiscordLloadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()ib:Notification("Notification", "Executed", "COOL")
+end)
+
+local btns = serv:Channel("Tools & FE")
+
+btns:Button("Keyboard", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt", true))()
+DiscordLloadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()ib:Notification("Notification", "Executed", "COOL")
+end)
+
+btns:Seperator()
+
+btns:Button("Fly", function()
+loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")()
+DiscordLloadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()ib:Notification("Notification", "Executed", "COOL")
+end)
+ 
+btns:Seperator()
+ 
+btns:Button("Infinite Yield", function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+DiscordLloadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()ib:Notification("Notification", "Executed", "COOL")
+end)
